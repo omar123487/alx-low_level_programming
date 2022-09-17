b970bf32204dc7c75ee5a81d79198d141fdba958
omar123487 <kideda123omar@gmail.com>
2022-09-17 09:57:13 -0700

    check 4 alphabetic xter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - check the code for alphabetic character.
+ *
+ * @c: letter being tested
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
